11fa41e7be45f6d2bfc0af55271c2c673bfdf5bd
alonespace <jomcootle@gmail.com>
2024-04-16 12:11:35 +0700

    ep9

---
+++ tranining/logic.dart
@@ -0,0 +1,56 @@
+void main() {
+  //* if else
+  int age = 15;
+  if (age > 18) {
+    print('Adult');
+  } else {
+    print('Child');
+  }
+
+  //* switch case
+  String grade = 'B';
+  switch (grade) {
+    case 'A':
+      print('Excellent');
+    case 'B':
+      print('Good');
+    case 'C':
+      print('Average');
+    default:
+      print('Fail');
+  }
+
+  // //* for loop
+  // for (int i = 0; i < 5; i++) {
+  //   print(i);
+  // }
+
+  // //* while loop
+  // int i = 0;
+  // while (i < 5) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // //* do while loop
+  // i = 0;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i < 5);
+
+  // //* break and continue
+  // for (int i = 0; i < 5; i++) {
+  //   if (i == 3) {
+  //     break;
+  //   }
+  //   print(i);
+  // }
+
+  // for (int i = 0; i < 5; i++) {
+  //   if (i == 3) {
+  //     continue;
+  //   }
+  //   print(i);
+  // }
+}
