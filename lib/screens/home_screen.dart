ed655a8953ab5a6e38d5dff34198e08cbe78dce2
alonespace <jomcootle@gmail.com>
2024-04-20 15:26:36 +0700

    ep21

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/lesson/ep20/my_button_widget.dart';
 import 'package:flutter_application/lesson/ep19/my_text_widget.dart';
+import 'package:flutter_application/lesson/ep21/my_image_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,7 +16,7 @@ class HomeScreen extends StatelessWidget {
       )),
       body: const Center(
         child: Column(
-          children: [MyTextWidget(), MyButtonsWidget()],
+          children: [MyTextWidget(), MyButtonsWidget(), MyImageWidget()],
         ),
       ),
     );
