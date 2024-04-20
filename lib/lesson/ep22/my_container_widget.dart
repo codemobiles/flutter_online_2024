77d13b872d29059f835df6c4507d8a9d3497f4f4
alonespace <jomcootle@gmail.com>
2024-04-20 16:03:31 +0700

    ep22

--- lib/lesson/ep22/my_container_widget.dart
+++ lib/lesson/ep22/my_container_widget.dart
@@ -7,19 +7,26 @@ class MyContainerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        decoration: BoxDecoration(
-          color: Colors.amber[600],
-          border: Border.all(
-            color: Colors.black,
-            width: 1,
+          decoration: BoxDecoration(
+            color: Colors.amber[600],
+            border: Border.all(
+              color: Colors.black,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(12),
           ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        margin: const EdgeInsets.all(10.0),
-        width: 128.0,
-        height: 128.0,
-        child: Text('Container Widget'),
-      ),
+          margin: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 60.0),
+          // width: 128.0,
+          // height: 128.0,
+          child: const Text(
+            'Container',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
     );
   }
 }
