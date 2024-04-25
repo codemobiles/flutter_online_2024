c87f52852f82fc7fc0fe9f5974774876a9c6087d
alonespace <jomcootle@gmail.com>
2024-04-25 23:07:41 +0700

    ep27

---
+++ lib/lesson/ep27/my_icon_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyIconWidget extends StatelessWidget {
+  const MyIconWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Icon(
+          Icons.favorite,
+          color: Colors.pink,
+          size: 24.0,
+          semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+          Icons.audiotrack,
+          color: Colors.green,
+          size: 30.0,
+        ),
+        Icon(
+          Icons.beach_access,
+          color: Colors.blue,
+          size: 36.0,
+        ),
+      ],
+    );
+  }
+}
